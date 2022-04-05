b009b2834b948be398fa0957c39b45084a085612
Ryan Liptak <squeek502@hotmail.com>
2022-04-04 23:37:43 -0700

    Add context menu integration

--- src/com.zig
+++ src/com.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const windows = std.os.windows;
+const windows_extra = @import("windows_extra.zig");
 const testing = std.testing;
 
 pub const IUnknown = extern struct {
@@ -80,6 +81,200 @@ pub const IClassFactory = extern struct {
         };
     }
 };
+
+pub const IShellExtInit = extern struct {
+    vtable: *extern struct {
+        unknown: IUnknown.VTable(IShellExtInit),
+        shell_ext: VTable(IShellExtInit),
+    },
+
+    const IID_Value = windows.GUID.parse("{000214e8-0000-0000-c000-000000000046}");
+    pub const IID = &IID_Value;
+
+    pub fn VTable(comptime T: type) type {
+        return extern struct {
+            Initialize: fn (
+                self: *T,
+                pidlFolder: windows_extra.PCIDLIST_ABSOLUTE,
+                pdtobj: ?*IDataObject,
+                hkeyProgID: windows.HKEY,
+            ) callconv(windows.WINAPI) windows.HRESULT,
+        };
+    }
+};
+
+pub const IContextMenu = extern struct {
+    vtable: *extern struct {
+        unknown: IUnknown.VTable(IContextMenu),
+        context_menu: VTable(IContextMenu),
+    },
+
+    const IID_Value = windows.GUID.parse("{000214e4-0000-0000-c000-000000000046}");
+    pub const IID = &IID_Value;
+
+    pub fn VTable(comptime T: type) type {
+        return extern struct {
+            QueryContextMenu: fn (
+                self: *T,
+                hmenu: ?windows.HMENU,
+                indexMenu: windows.UINT,
+                idCmdFirst: windows.UINT,
+                idCmdLast: windows.UINT,
+                uFlags: windows.UINT,
+            ) callconv(windows.WINAPI) windows.HRESULT,
+            InvokeCommand: fn (
+                self: *T,
+                pici: ?*CMINVOKECOMMANDINFO,
+            ) callconv(windows.WINAPI) windows.HRESULT,
+            GetCommandString: fn (
+                self: *T,
+                idCmd: windows_extra.UINT_PTR,
+                uType: GCS,
+                pReserved: ?*windows.UINT,
+                pszName: ?[*]windows.CHAR,
+                cchMax: windows.UINT,
+            ) callconv(windows.WINAPI) windows.HRESULT,
+        };
+    }
+};
+
+// QueryContextMenu uFlags
+pub const CMF_NORMAL: windows.UINT = 0x00000000;
+pub const CMF_DEFAULTONLY: windows.UINT = 0x00000001;
+pub const CMF_VERBSONLY: windows.UINT = 0x00000002;
+pub const CMF_EXPLORE: windows.UINT = 0x00000004;
+pub const CMF_NOVERBS: windows.UINT = 0x00000008;
+pub const CMF_CANRENAME: windows.UINT = 0x00000010;
+pub const CMF_NODEFAULT: windows.UINT = 0x00000020;
+pub const CMF_INCLUDESTATIC: windows.UINT = 0x00000040;
+pub const CMF_ITEMMENU: windows.UINT = 0x00000080;
+pub const CMF_EXTENDEDVERBS: windows.UINT = 0x00000100;
+pub const CMF_DISABLEDVERBS: windows.UINT = 0x00000200;
+pub const CMF_ASYNCVERBSTATE: windows.UINT = 0x00000400;
+pub const CMF_OPTIMIZEFORINVOKE: windows.UINT = 0x00000800;
+pub const CMF_SYNCCASCADEMENU: windows.UINT = 0x00001000;
+pub const CMF_DONOTPICKDEFAULT: windows.UINT = 0x00002000;
+pub const CMF_RESERVED: windows.UINT = 0xffff0000;
+
+// GetCommandString uType
+pub const GCS = enum(windows.UINT) {
+    VERBA = 0x00000000,
+    HELPTEXTA = 0x00000001,
+    VALIDATEA = 0x00000002,
+    VERBW = 0x00000004,
+    HELPTEXTW = 0x00000005,
+    VALIDATEW = 0x00000006,
+    VERBICONW = 0x00000014,
+};
+pub const GCS_VERBA = GCS.VERBA;
+pub const GCS_HELPTEXTA = GCS.HELPTEXTA;
+pub const GCS_VALIDATEA = GCS.VALIDATEA;
+pub const GCS_VERBW = GCS.VERBW;
+pub const GCS_HELPTEXTW = GCS.HELPTEXTW;
+pub const GCS_VALIDATEW = GCS.VALIDATEW;
+pub const GCS_VERBICONW = GCS.VERBICONW;
+
+pub const CMINVOKECOMMANDINFO = extern struct {
+    cbSize: windows.DWORD,
+    fMask: windows.DWORD,
+    hwnd: ?windows.HWND,
+    lpVerb: ?windows.LPCSTR,
+    lpParameters: ?windows.LPCSTR,
+    lpDirectory: ?windows.LPCSTR,
+    nShow: c_int,
+    dwHotKey: windows.DWORD,
+    hIcon: ?windows.HANDLE,
+};
+
+pub const IDataObject = extern struct {
+    vtable: *extern struct {
+        unknown: IUnknown.VTable(IDataObject),
+        data_object: VTable(IDataObject),
+    },
+
+    const IID_Value = windows.GUID.parse("{0000010e-0000-0000-C000-000000000046}");
+    pub const IID = &IID_Value;
+
+    pub fn VTable(comptime T: type) type {
+        return extern struct {
+            GetData: fn (
+                self: *T,
+                pformatetcIn: ?*FORMATETC,
+                pmedium: ?*STGMEDIUM,
+            ) callconv(windows.WINAPI) windows.HRESULT,
+            GetDataHere: *anyopaque,
+            QueryGetData: *anyopaque,
+            GetCanonicalFormatEtc: *anyopaque,
+            SetData: *anyopaque,
+            EnumFormatEtc: *anyopaque,
+            DAdvise: *anyopaque,
+            DUnadvise: *anyopaque,
+            EnumDAdvise: *anyopaque,
+        };
+    }
+};
+
+pub const TYMED = enum(u32) {
+    HGLOBAL = 1,
+    FILE = 2,
+    ISTREAM = 4,
+    ISTORAGE = 8,
+    GDI = 16,
+    MFPICT = 32,
+    ENHMF = 64,
+    NULL = 0,
+};
+pub const TYMED_HGLOBAL = TYMED.HGLOBAL;
+pub const TYMED_FILE = TYMED.FILE;
+pub const TYMED_ISTREAM = TYMED.ISTREAM;
+pub const TYMED_ISTORAGE = TYMED.ISTORAGE;
+pub const TYMED_GDI = TYMED.GDI;
+pub const TYMED_MFPICT = TYMED.MFPICT;
+pub const TYMED_ENHMF = TYMED.ENHMF;
+pub const TYMED_NULL = TYMED.NULL;
+
+pub const STGMEDIUM = extern struct {
+    tymed: TYMED,
+    Anonymous: extern union {
+        hBitmap: ?*anyopaque,
+        hMetaFilePict: ?*anyopaque,
+        hEnhMetaFile: ?*anyopaque,
+        hGlobal: ?windows_extra.HGLOBAL,
+        lpszFileName: ?windows.PWSTR,
+        pstm: ?*anyopaque,
+        pstg: ?*anyopaque,
+    },
+    pUnkForRelease: ?*IUnknown,
+};
+pub extern "ole32" fn ReleaseStgMedium(stg: ?*STGMEDIUM) callconv(windows.WINAPI) void;
+
+pub const DVTARGETDEVICE = extern struct {
+    tdSize: u32,
+    tdDriverNameOffset: u16,
+    tdDeviceNameOffset: u16,
+    tdPortNameOffset: u16,
+    tdExtDevmodeOffset: u16,
+    tdData: [1]u8,
+};
+
+pub const FORMATETC = extern struct {
+    cfFormat: windows_extra.CLIPBOARD_FORMATS,
+    ptd: ?*DVTARGETDEVICE,
+    dwAspect: DVASPECT,
+    lindex: i32,
+    tymed: TYMED,
+};
+
+pub const DVASPECT = enum(u32) {
+    CONTENT = 1,
+    THUMBNAIL = 2,
+    ICON = 4,
+    DOCPRINT = 8,
+};
+pub const DVASPECT_CONTENT = DVASPECT.CONTENT;
+pub const DVASPECT_THUMBNAIL = DVASPECT.THUMBNAIL;
+pub const DVASPECT_ICON = DVASPECT.ICON;
+pub const DVASPECT_DOCPRINT = DVASPECT.DOCPRINT;
 
 pub fn IsEqualGUID(a: *const windows.GUID, b: *const windows.GUID) bool {
     return std.meta.eql(a.*, b.*);
--- src/windows_extra.zig
+++ src/windows_extra.zig
@@ -1,4 +1,86 @@
-const windows = @import("std").os.windows;
+const std = @import("std");
+const windows = std.os.windows;
+
+pub const HGLOBAL = windows.HANDLE;
+
+pub extern "kernel32" fn GlobalLock(hMem: HGLOBAL) callconv(windows.WINAPI) ?windows.LPVOID;
+pub extern "kernel32" fn GlobalUnlock(hMem: HGLOBAL) callconv(windows.WINAPI) windows.BOOL;
+
+pub const HDROP = *opaque {};
+
+pub extern "shell32" fn DragQueryFileW(
+    hDrop: HDROP,
+    iFile: windows.UINT,
+    lpszFile: ?windows.LPWSTR,
+    cch: windows.UINT,
+) callconv(windows.WINAPI) windows.UINT;
+// https://docs.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-dragqueryfilew
+// If the value of this parameter is 0xFFFFFFFF, DragQueryFile returns a count of the files dropped.
+pub const DragQueryFile_GetCount: windows.UINT = 0xFFFFFFFF;
+
+pub extern "shell32" fn SHChangeNotify(
+    wEventId: windows.LONG,
+    uFlags: windows.UINT,
+    dwItem1: ?windows.LPCVOID,
+    dwItem2: ?windows.LPCVOID,
+) callconv(windows.WINAPI) void;
+
+pub const SHCNE_UPDATEITEM: windows.LONG = 0x00002000;
+pub const SHCNF_PATHW: windows.UINT = 0x0005;
+
+const UnsignedHRESULT = std.meta.Int(.unsigned, @typeInfo(windows.HRESULT).Int.bits);
+pub fn MAKE_HRESULT(severity: u1, facility: u16, code: u16) windows.HRESULT {
+    var hr: UnsignedHRESULT = (@as(UnsignedHRESULT, severity) << 31) | (@as(UnsignedHRESULT, facility) << 16) | code;
+    return @bitCast(windows.HRESULT, hr);
+}
+pub fn HRESULT_CODE(hr: windows.HRESULT) u16 {
+    return @intCast(u16, @bitCast(UnsignedHRESULT, hr) & 0xFFFF);
+}
+pub fn HRESULT_FACILITY(hr: windows.HRESULT) u16 {
+    return @intCast(u16, (@bitCast(UnsignedHRESULT, hr) >> 16) & 0x1FFF);
+}
+pub fn HRESULT_SEVERITY(hr: windows.HRESULT) u1 {
+    return @intCast(u1, (@bitCast(UnsignedHRESULT, hr) >> 31) & 0x1);
+}
+pub fn HRESULT_FROM_WIN32(err: windows.Win32Error) windows.HRESULT {
+    var hr: UnsignedHRESULT = (@as(UnsignedHRESULT, @enumToInt(err)) & 0x0000FFFF) | (@as(UnsignedHRESULT, FACILITY_WIN32) << 16) | @as(UnsignedHRESULT, 0x80000000);
+    return @bitCast(windows.HRESULT, hr);
+}
+
+test "HRESULT" {
+    const hr = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_NULL, 5);
+    try std.testing.expectEqual(SEVERITY_ERROR, HRESULT_SEVERITY(hr));
+    try std.testing.expectEqual(FACILITY_NULL, HRESULT_FACILITY(hr));
+    try std.testing.expectEqual(@as(u16, 5), HRESULT_CODE(hr));
+}
+
+pub const SEVERITY_SUCCESS: u1 = 0;
+pub const SEVERITY_ERROR: u1 = 1;
+
+pub const FACILITY_NULL: u16 = 0;
+pub const FACILITY_WIN32: u16 = 7;
+
+pub fn HIWORD(x: windows.DWORD) windows.WORD {
+    return @intCast(windows.WORD, (x >> 16) & 0xFFFF);
+}
+
+pub fn LOWORD(x: windows.DWORD) windows.WORD {
+    return @intCast(windows.WORD, x & 0xFFFF);
+}
+
+// An unsigned integer, whose length is dependent on processor word size.
+// https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-tsts/f959534d-51f2-4103-8fb5-812620efe49b
+pub const UINT_PTR = usize;
+
+pub extern "user32" fn InsertMenuW(
+    hMenu: ?windows.HMENU,
+    uPosition: windows.UINT,
+    uFlags: windows.UINT,
+    uIDNewItem: UINT_PTR,
+    lpNewItem: ?windows.LPCWSTR,
+) callconv(windows.WINAPI) windows.BOOL;
+
+pub const MF_BYPOSITION: windows.UINT = 0x00000400;
 
 pub const ISIOI_ICONFILE: windows.DWORD = 0x1;
 pub const ISIOI_ICONINDEX: windows.DWORD = 0x2;
@@ -13,10 +95,70 @@ pub const DLL_PROCESS_DETACH: windows.DWORD = 0;
 pub const DLL_THREAD_ATTACH: windows.DWORD = 2;
 pub const DLL_THREAD_DETACH: windows.DWORD = 3;
 
-pub extern "kernel32" fn InterlockedIncrement(
-    Addend: ?*volatile windows.LONG,
-) callconv(windows.WINAPI) windows.LONG;
+pub const SHITEMID = extern struct {
+    cb: windows.USHORT,
+    abID: [1]u8,
+};
 
-pub extern "kernel32" fn InterlockedDecrement(
-    Addend: ?*volatile windows.LONG,
-) callconv(windows.WINAPI) windows.LONG;
+pub const ITEMIDLIST = extern struct {
+    mkid: SHITEMID,
+};
+
+pub const PCIDLIST_ABSOLUTE = ?*const ITEMIDLIST;
+
+pub const CLIPBOARD_FORMATS = enum(u16) {
+    TEXT = 1,
+    BITMAP = 2,
+    METAFILEPICT = 3,
+    SYLK = 4,
+    DIF = 5,
+    TIFF = 6,
+    OEMTEXT = 7,
+    DIB = 8,
+    PALETTE = 9,
+    PENDATA = 10,
+    RIFF = 11,
+    WAVE = 12,
+    UNICODETEXT = 13,
+    ENHMETAFILE = 14,
+    HDROP = 15,
+    LOCALE = 16,
+    DIBV5 = 17,
+    MAX = 18,
+    OWNERDISPLAY = 128,
+    DSPTEXT = 129,
+    DSPBITMAP = 130,
+    DSPMETAFILEPICT = 131,
+    DSPENHMETAFILE = 142,
+    PRIVATEFIRST = 512,
+    PRIVATELAST = 767,
+    GDIOBJFIRST = 768,
+    GDIOBJLAST = 1023,
+};
+pub const CF_TEXT = CLIPBOARD_FORMATS.TEXT;
+pub const CF_BITMAP = CLIPBOARD_FORMATS.BITMAP;
+pub const CF_METAFILEPICT = CLIPBOARD_FORMATS.METAFILEPICT;
+pub const CF_SYLK = CLIPBOARD_FORMATS.SYLK;
+pub const CF_DIF = CLIPBOARD_FORMATS.DIF;
+pub const CF_TIFF = CLIPBOARD_FORMATS.TIFF;
+pub const CF_OEMTEXT = CLIPBOARD_FORMATS.OEMTEXT;
+pub const CF_DIB = CLIPBOARD_FORMATS.DIB;
+pub const CF_PALETTE = CLIPBOARD_FORMATS.PALETTE;
+pub const CF_PENDATA = CLIPBOARD_FORMATS.PENDATA;
+pub const CF_RIFF = CLIPBOARD_FORMATS.RIFF;
+pub const CF_WAVE = CLIPBOARD_FORMATS.WAVE;
+pub const CF_UNICODETEXT = CLIPBOARD_FORMATS.UNICODETEXT;
+pub const CF_ENHMETAFILE = CLIPBOARD_FORMATS.ENHMETAFILE;
+pub const CF_HDROP = CLIPBOARD_FORMATS.HDROP;
+pub const CF_LOCALE = CLIPBOARD_FORMATS.LOCALE;
+pub const CF_DIBV5 = CLIPBOARD_FORMATS.DIBV5;
+pub const CF_MAX = CLIPBOARD_FORMATS.MAX;
+pub const CF_OWNERDISPLAY = CLIPBOARD_FORMATS.OWNERDISPLAY;
+pub const CF_DSPTEXT = CLIPBOARD_FORMATS.DSPTEXT;
+pub const CF_DSPBITMAP = CLIPBOARD_FORMATS.DSPBITMAP;
+pub const CF_DSPMETAFILEPICT = CLIPBOARD_FORMATS.DSPMETAFILEPICT;
+pub const CF_DSPENHMETAFILE = CLIPBOARD_FORMATS.DSPENHMETAFILE;
+pub const CF_PRIVATEFIRST = CLIPBOARD_FORMATS.PRIVATEFIRST;
+pub const CF_PRIVATELAST = CLIPBOARD_FORMATS.PRIVATELAST;
+pub const CF_GDIOBJFIRST = CLIPBOARD_FORMATS.GDIOBJFIRST;
+pub const CF_GDIOBJLAST = CLIPBOARD_FORMATS.GDIOBJLAST;
